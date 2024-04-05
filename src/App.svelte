a8e127c500365f75d1c0b26caccb5947173401c2
Steven9101 <164558083+Steven9101@users.noreply.github.com>
2024-04-05 12:03:53 +0200

    Add files via upload

--- src/App.svelte
+++ src/App.svelte
@@ -313,7 +313,7 @@
     audio.setMute(mute)
   }
   function handleVolumeChange () {
-    audio.setGain(Math.pow(10, (volume - 50) / 50 + 3.8))
+    audio.setGain(Math.pow(10, (volume - 50) / 50 + 1.8))
   }
   function handleSquelchChange () {
     squelchEnable = !squelchEnable
